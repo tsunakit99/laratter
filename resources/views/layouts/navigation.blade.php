40f70ac2aedfbd9dc77d81419da3de6098e0af55
tsunakit99 <tainosuke0926@gmail.com>
2023-09-05 20:16:20 +0900

    tweet実装

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,18 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <!-- 🔽 一覧ページへのリンクを追加 -->
+        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+          <x-nav-link :href="route('tweet.index')" :active="request()->routeIs('tweet.index')">
+            {{ __('Index') }}
+          </x-nav-link>
+        </div>
+        <!-- 🔽 作成ページへのリンクを追加 -->
+        <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+          <x-nav-link :href="route('tweet.create')" :active="request()->routeIs('tweet.create')">
+            {{ __('Create') }}
+          </x-nav-link>
+        </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -71,6 +83,18 @@
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
+        <!-- 🔽 一覧ページへのリンクを追加 -->
+    <div class="pt-2 pb-3 space-y-1">
+      <x-responsive-nav-link :href="route('tweet.index')" :active="request()->routeIs('tweet.index')">
+        {{ __('Index') }}
+      </x-responsive-nav-link>
+    </div>
+    <!-- 🔽 作成ページへのリンクを追加 -->
+    <div class="pt-2 pb-3 space-y-1">
+      <x-responsive-nav-link :href="route('tweet.create')" :active="request()->routeIs('tweet.create')">
+        {{ __('Create') }}
+      </x-responsive-nav-link>
+    </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
